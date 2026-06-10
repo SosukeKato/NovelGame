e36dcf50e7d78e204069710a81e8b6647977728a
SosukeKato <dokindokinpiano@gmail.com>
2026-06-10 17:49:03 +0900

    [fix]クラス名をChapterNovelScenarioへ修正

---
+++ Assets/_WorkSpace/Script/ChapterNovelScenario.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class NovelScenario
+{
+
+}
+
+[CreateAssetMenu(menuName = "GameData/Scenario",fileName = "NewScenarioData")]
+public class ChapterNovelScenario : ScriptableObject
+{
+
+}
--- Assets/_WorkSpace/Script/ScenarioDataBase.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "GameData/Scenario",fileName = "NewScenarioData")]
-public class ScenarioDataBase : ScriptableObject
-{
-
-}
