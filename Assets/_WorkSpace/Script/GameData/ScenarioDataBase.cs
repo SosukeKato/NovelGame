2b14e80d6ff11f415788ce1f0f64ae82ffa981be
SosukeKato <dokindokinpiano@gmail.com>
2026-07-09 03:49:02 +0900

    [update]Dictionary初期化処理

--- Assets/_WorkSpace/Script/GameData/ScenarioDataBase.cs
+++ Assets/_WorkSpace/Script/GameData/ScenarioDataBase.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "GameData/ScenarioDataBase", fileName = "NewScenarioDataBase")]
 public class ScenarioDataBase : ScriptableObject
 {
-    
+    [SerializeField] private ChapterNovelScenario[] _chapters;
+
+    private Dictionary<int, ChapterNovelScenario> _chapterDictionary;
+
+    public void InitDictionary()
+    {
+        _chapterDictionary = new Dictionary<int, ChapterNovelScenario>();
+        foreach (ChapterNovelScenario chapter in _chapters)
+        {
+            _chapterDictionary[chapter.id] = chapter;
+        }
+    }
 }
