ce144dd5a5d6cd594756cee2c3872cb9f0c79461
SosukeKato <dokindokinpiano@gmail.com>
2026-07-15 04:30:18 +0900

    [update]Title画面の遷移を追加

--- Assets/_WorkSpace/Script/UIButton.cs
+++ Assets/_WorkSpace/Script/UIButton.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Header("1st")] GameObject _firstUI;
+    [SerializeField, Header("2nd")] GameObject _secondUI;
+    [SerializeField, Header("3rd")] GameObject _thirdUI;
+
+    /// <summary>
+    /// チャプターセレクトへ移行
+    /// </summary>
+    /// <param name="before"></param>
+    /// <param name="after"></param>
+    public void ProceedChapterSelect()
     {
-        
+        _firstUI.SetActive(false);
+        _secondUI.SetActive(true);
+    }
+    
+    /// <summary>
+    /// スタート画面へ移行
+    /// </summary>
+    /// <param name="before"></param>
+    /// <param name="after"></param>
+    /// <param name="id"></param>
+    public void ProceedWaitStart(int id)
+    {
+        _secondUI.SetActive(false);
+        _thirdUI.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Chapterを開始する
+    /// </summary>
+    /// <param name="id"></param>
+    public void ProceedScenarioScene(int id)
     {
-        
+        GameManager.instance.GameData.CurrentChapterID = id;
+        SceneController.instance.LoadScenario();
     }
 }
