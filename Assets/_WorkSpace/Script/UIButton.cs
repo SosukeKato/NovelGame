6987f63e86017b401942d27bc4ee2f0c535838f7
SosukeKato <dokindokinpiano@gmail.com>
2026-07-15 04:59:21 +0900

    [update]キャラクタ説明欄に説明が表示される機能を実装

--- Assets/_WorkSpace/Script/UIButton.cs
+++ Assets/_WorkSpace/Script/UIButton.cs
@@ -1,10 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIButton : MonoBehaviour
 {
+    [SerializeField, Header("ディクショナリ")] ScenarioDataBase _scenarioDataBase;
     [SerializeField, Header("1st")] GameObject _firstUI;
     [SerializeField, Header("2nd")] GameObject _secondUI;
     [SerializeField, Header("3rd")] GameObject _thirdUI;
+    [SerializeField, Header("キャラクターの見た目反映用Image")] Image _characterImageBox;
+    [SerializeField, Header("キャラクターの説明反映用Text")] TextMeshProUGUI _characterInfomationBox;
 
     /// <summary>
     /// チャプターセレクトへ移行
@@ -25,7 +30,10 @@ public class UIButton : MonoBehaviour
     /// <param name="id"></param>
     public void ProceedWaitStart(int id)
     {
+        CharacterData characterData = _scenarioDataBase.GetChapter(id).Character;
         _secondUI.SetActive(false);
+        _characterInfomationBox.text = characterData.CharacterInfomation;
+        _characterImageBox.sprite = characterData.CharacterIamge;
         _thirdUI.SetActive(true);
     }
 
