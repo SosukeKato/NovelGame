1446fb0c6698761e948cb8d184a979b22f2a9ec6
SosukeKato <dokindokinpiano@gmail.com>
2026-07-24 21:52:08 +0900

    [fix]保持するデータを修正

--- Assets/_WorkSpace/Script/GameData/ChapterNovelScenario.cs
+++ Assets/_WorkSpace/Script/GameData/ChapterNovelScenario.cs
@@ -23,11 +23,15 @@ public class CharacterData
 [System.Serializable]
 public class BattleData
 {
-    public AudioClip BGM;
-    public Sprite EnemyImage;
+    public AudioClip BattleBGM;
+    public Sprite BattleEnemyImage;
     public Sprite CharacterImage;
-    public string[] CharacterAction;
-    public bool[] IsCorrectOption;
+    public string CharacterAction;
+    public string AcceptText;
+    public string DeclineText;
+    public int AnalysisUpAmount;
+    public int DangerUpAmount;
+    public bool IsAcceptCorrect;
 }
 
 [CreateAssetMenu(menuName = "GameData/Scenario",fileName = "NewScenarioData")]
@@ -35,6 +39,6 @@ public class ChapterNovelScenario : ScriptableObject
 {
     public int ID;
     public CharacterData Character;
-    public BattleData Battle;
+    public BattleData[] Battle;
     public Scenario[] Scenario;
 }
