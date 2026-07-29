011d0772034b5ac9e058939b55e4ca4bab1db843
SosukeKato <dokindokinpiano@gmail.com>
2026-07-29 17:21:03 +0900

    [update]バトルUI更新処理を追加

--- Assets/_WorkSpace/Script/Manager/BattleManager.cs
+++ Assets/_WorkSpace/Script/Manager/BattleManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class BattleManager : MonoBehaviour
     public int CurrentDangerLevel => _currentDangerLevel;      //BattleSceneã‚ÌUIŽÀ‘•‚ÉŽg—p
 
     [SerializeField] ScenarioDataBase _scenarioDataBase;
+
+    [SerializeField] Image _analysisLevelGauge;
+    [SerializeField] Image _dangerLevelGauge;
 
     ChapterNovelScenario _chapter;
     int _currentAnalysisLevel;
@@ -47,8 +51,16 @@ public class BattleManager : MonoBehaviour
         BattleData data = _chapter.Battle[_currentBattleTurn];
         bool isCorrect = data.IsAcceptCorrect == isAccept;
 
-        if (isCorrect) _currentAnalysisLevel += data.AnalysisUpAmount;
-        else _currentDangerLevel += data.DangerUpAmount;
+        if (isCorrect)
+        {
+            _currentAnalysisLevel += data.AnalysisUpAmount;
+            UIManager.Instance.UpdateGauge(_analysisLevelGauge, _currentAnalysisLevel);
+        }
+        else
+        {
+            _currentDangerLevel += data.DangerUpAmount;
+            UIManager.Instance.UpdateGauge(_dangerLevelGauge, _currentDangerLevel);
+        }
 
         if (_currentAnalysisLevel >= PARAMETER_MAX) BattleResultTransfer(true);
         else if (_currentDangerLevel >= PARAMETER_MAX) BattleResultTransfer(false);
