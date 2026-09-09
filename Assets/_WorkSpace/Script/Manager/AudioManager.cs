0de4d970f71b940af6d4f3a4738bf0eea8a1bf14
SosukeKato <dokindokinpiano@gmail.com>
2026-09-10 03:22:55 +0900

    [update]Audio再生処理追加

--- Assets/_WorkSpace/Script/Manager/AudioManager.cs
+++ Assets/_WorkSpace/Script/Manager/AudioManager.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager instance { get; set; }
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// BGMçƒê∂
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="bgm"></param>
+    public void PlayBGM(AudioSource source, AudioClip bgm)
     {
-        
+        source.clip = bgm;
+        source.Play();
+    }
+
+    /// <summary>
+    /// BGMèIóπ
+    /// </summary>
+    /// <param name="source"></param>
+    public void StopBGM(AudioSource source)
+    {
+        source?.Stop();
+    }
+
+    /// <summary>
+    /// SEçƒê∂
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="se"></param>
+    public void PlaySE(AudioSource source, AudioClip se)
+    {
+        source.PlayOneShot(se);
     }
 }
